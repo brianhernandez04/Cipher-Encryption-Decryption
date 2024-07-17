e03ef48c1c06803147866e2309a435149935d92e
Brian Hernandez <hernandezbriannnn@gmail.com>
2024-07-17 00:16:24 -0700

    Completed Cipher

--- cipher.py
+++ cipher.py
@@ -1,15 +1,45 @@
+# List that stores the letters needed. Alphabet was inputted twice in order to account for the Invalid Range error that occured.
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+cipher_complete = False
 
-def encrypt(text, shift):
-    cipher_text = "" # Created an empty string to add each of the letteers from the loop below.
-    for letters in text:
-        position = alphabet.index(letters)
-        new_position = position + shift
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded test is {cipher_text}")
-# There was an out of range error when hitting the last letter in the alphabet. All you needed to do was just copy/paste the alphabet again in the alphabet string.....    
-encrypt(text, shift)
+while cipher_complete == False:
+# Inputs which will be called later in the code to sort through the list above and either encode/decode the 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))    
+
+# Function to encrypt the message
+    def encrypt(plain_text, shift_amount):
+        cipher_text = ""
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            cipher_text += alphabet[new_position]
+        print(f"The encoded text is {cipher_text}")
+
+# Function to decrypt the message
+    def decrypt(textmsg, secondshift):
+        decoded_text = ""
+        for items in textmsg:
+            secposition = alphabet.index(items)
+            newest_position = secposition - secondshift
+            decoded_text += alphabet[newest_position]
+        print(f"The decoded text is {decoded_text}") 
+    
+
+    def choice():
+        global cipher_complete # Global specifies the variable outside this for loop. This is needed to escape the cipher
+        choice = input("Would you like to run this cipher again? Type 'yes' or 'no':\n").lower()
+        if choice == "no":
+            print("The cipher program has now completed.")
+            cipher_complete = True
+            
+
+
+    if direction == "encode":
+        encrypt(plain_text=text, shift_amount=shift)
+        choice()   
+    else:
+        decrypt(textmsg=text, secondshift=shift)
+        choice()
